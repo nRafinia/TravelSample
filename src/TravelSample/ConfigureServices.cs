6baff67aeb475018dcad48112908c3e1ae03a9a3
Naser <naser.r@gmail.com>
2024-05-29 18:40:34 +0330

    Add Dependency Injection and Configuration services
    This commit introduces a new ConfigureServices class to register DI services and Configuration settings. The Program file has also been modified to use these new services. Furthermore, additional project references and package dependencies required for DI and Configuration have been added to the csproj file.

---
+++ src/TravelSample/ConfigureServices.cs
@@ -0,0 +1,16 @@
+﻿
+
+// ReSharper disable InconsistentNaming
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TravelSample;
+
+public static class ConfigureServices
+{
+    public static void Register(IServiceCollection services, IConfiguration configuration)
+    {
+
+    }
+}
--- src/TravelSample/Program.cs
+++ src/TravelSample/Program.cs
@@ -1,3 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+var serviceCollection = new ServiceCollection()
+    .AddLogging();
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional:true , reloadOnChange:true)
+    .AddEnvironmentVariables()
+    .AddCommandLine(args)
+    .Build();
+
+TravelSample.Application.ConfigureServices.Register(serviceCollection, configuration);
+TravelSample.Infra.ConfigureServices.Register(serviceCollection, configuration);
+TravelSample.ConfigureServices.Register(serviceCollection, configuration);
+
+var serviceProvider = serviceCollection
+    .BuildServiceProvider();
+
 Console.WriteLine("Hello, World!");
+
