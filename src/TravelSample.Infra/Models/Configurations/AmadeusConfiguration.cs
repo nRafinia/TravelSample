b29685d22a013ac2de0fba5afa7b7ccc7e659468
Naser <naser.r@gmail.com>
2024-05-29 19:46:32 +0330

    Add Amadeus and Sabre configuration classes
    Added two new configuration classes, AmadeusConfiguration and SabreConfiguration, in the Models.Configurations namespace. These classes will be used to hold the configurations for their respective service APIs.

---
+++ src/TravelSample.Infra/Models/Configurations/AmadeusConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace TravelSample.Infra.Models.Configurations;
+
+public class AmadeusConfiguration
+{
+    public string Domain { get; set; }
+    public string Key { get; set; }
+    public string Secret { get; set; }
+}
---
+++ src/TravelSample.Infra/Models/Configurations/SabreConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace TravelSample.Infra.Models.Configurations;
+
+public class SabreConfiguration
+{
+    public string Domain { get; set; }
+    public string ApiUserId { get; set; }
+    public string ApiPassword { get; set; }
+}
