8c9bcac4d2d0c8b1c9b58317aa429b1f3d17df93
Naser <naser.r@gmail.com>
2024-05-29 20:00:34 +0330

    Add Sabre and Amadeus services configuration
    This commit integrates the Sabre and Amadeus services into the application. The ConfigureServices class is updated to include configurations for both services along with Refit.Httpclients configuration and service registration. This allows interaction with the Sabre and Amadeus APIs.

--- src/TravelSample.Infra/ConfigureServices.cs
+++ src/TravelSample.Infra/ConfigureServices.cs
@@ -1,34 +1,62 @@
-﻿
-
-// ReSharper disable InconsistentNaming
+﻿// ReSharper disable InconsistentNaming
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Refit;
+using TravelSample.Domain.Abstractions;
+using TravelSample.Infra.Extensions;
+using TravelSample.Infra.Handler;
+using TravelSample.Infra.Models.Configurations;
+using TravelSample.Infra.Providers;
+using TravelSample.Infra.Services;
 
 namespace TravelSample.Infra;
 
 public static class ConfigureServices
 {
-    //private const string WHMCSConfigurationName = nameof(WHMCSConfiguration);
+    private const string SabreConfigurationName = nameof(SabreConfiguration);
+    private const string AmadeusConfigurationName = nameof(AmadeusConfiguration);
 
     public static void Register(IServiceCollection services, IConfiguration configuration)
     {
+        services.AddSingleton<ITravelService, TravelService>();
 
-        
         AddRefitServices(services, configuration);
     }
 
     private static void AddRefitServices(IServiceCollection services, IConfiguration configuration)
     {
-        /*var config = configuration.RegisterAndGetConfiguration<WHMCSConfiguration>(services, WHMCSConfigurationName);
+        AddSabreRefitService(services, configuration);
+        AddAmadeusRefitService(services, configuration);
+    }
 
-        services.AddScoped<SentryHttpMessageHandler>();
-        services.AddScoped<CollectionIndexerUrlHandler>();
-        
+    private static void AddSabreRefitService(IServiceCollection services, IConfiguration configuration)
+    {
+        var sabreConfiguration =
+            configuration.RegisterAndGetConfiguration<SabreConfiguration>(services, SabreConfigurationName);
+
         services
-            .AddRefitClient<IRequestService>()
-            .ConfigureHttpClient(c => c.BaseAddress = new Uri(config.Server))
-            .AddHttpMessageHandler<CollectionIndexerUrlHandler>()
-            .AddHttpMessageHandler<SentryHttpMessageHandler>();*/
+            .AddRefitClient<ISabreTokenService>()
+            .ConfigureHttpClient(c => c.BaseAddress = new Uri(sabreConfiguration.Domain));
+
+        services
+            .AddRefitClient<ISabreService>()
+            .ConfigureHttpClient(c => c.BaseAddress = new Uri(sabreConfiguration.Domain))
+            .AddHttpMessageHandler<SabreAuthenticationHandler>();
+    }
+
+    private static void AddAmadeusRefitService(IServiceCollection services, IConfiguration configuration)
+    {
+        var amadeusConfiguration =
+            configuration.RegisterAndGetConfiguration<AmadeusConfiguration>(services, AmadeusConfigurationName);
+
+        services
+            .AddRefitClient<IAmadeusTokenService>()
+            .ConfigureHttpClient(c => c.BaseAddress = new Uri(amadeusConfiguration.Domain));
+
+        services
+            .AddRefitClient<IAmadeusService>()
+            .ConfigureHttpClient(c => c.BaseAddress = new Uri(amadeusConfiguration.Domain))
+            .AddHttpMessageHandler<AmadeusAuthenticationHandler>();
     }
 }
