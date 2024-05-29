54342dd120f775a7e6f8b2958b0ebc2ac4e57c46
Naser <naser.r@gmail.com>
2024-05-29 22:26:27 +0330

    Add SearchFlight method to ISabreService interface
    Expanded the ISabreService interface to include a SearchFlight method. This new method leverages the Refit library for simplified HTTP communication, enabling flight search operations based on origin, destination, and date parameters.

--- src/TravelSample.Infra/Providers/ISabreService.cs
+++ src/TravelSample.Infra/Providers/ISabreService.cs
@@ -1,6 +1,10 @@
-﻿namespace TravelSample.Infra.Providers;
+﻿using Refit;
+using TravelSample.Infra.Models.Sabre;
+
+namespace TravelSample.Infra.Providers;
 
 public interface ISabreService
 {
-    
+    [Get("/v1/shop/flights?origin={origin}&destination={destination}&departuredate={departureDate}&returndate={returnDate}")]
+    Task<FlightResponse> SearchFlight(string origin, string destination, string departureDate, string returnDate);
 }
