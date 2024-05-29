eb71ff9036f9f486d2a6a0a8eda0b1adb9e550f6
Naser <naser.r@gmail.com>
2024-05-29 20:31:11 +0330

    Add Base64 encoding functionality to StringExt
    This commit introduces a ToBase64 extension method for strings. It uses the UTF8 encoding system to convert plain text into Base64 encoded strings. This method has been added to the StringExt static class in the TravelSample.Infrastructure.Extensions namespace.

---
+++ src/TravelSample.Infra/Extensions/StringExt.cs
@@ -0,0 +1,10 @@
+﻿namespace TravelSample.Infra.Extensions;
+
+internal static class StringExt
+{
+    public static string ToBase64(this string plainText) 
+    {
+        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+        return Convert.ToBase64String(plainTextBytes);
+    }
+}
