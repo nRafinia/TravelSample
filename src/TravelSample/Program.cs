a20a89b4208eb7f7facd671ba5a4259ec2978941
Naser <naser.r@gmail.com>
2024-05-29 22:30:24 +0330

    Add flight service logic and environment setting adjustments
    The commit includes the addition of a Flight service logic and adjustments on the handling of environment setting in the main Program.cs. The Flight service logic added will instantiate the FlightLogic class and call its Search method, enhancing the functionality of the application related to flight management. Additionally, the adjustment on environment setting handling improves the way the program deals with environment setup depending on the build configuration.

--- src/TravelSample/Program.cs
+++ src/TravelSample/Program.cs
@@ -2,12 +2,20 @@
 
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TravelSample.Application.Flights;
+using TravelSample.Application.Models.SearchFlights;
 
 var serviceCollection = new ServiceCollection()
     .AddLogging();
 
-var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-
+const string environmentName =
+#if DEBUG
+        "Development"
+#else
+    string.Empty
+#endif
+    ;
+    
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
@@ -22,5 +30,8 @@ TravelSample.ConfigureServices.Register(serviceCollection, configuration);
 
 var serviceProvider = serviceCollection
     .BuildServiceProvider();
+
+var flightService=serviceProvider.GetService<IFlightLogic>();
+var a = await flightService.Search(new SearchFlightRequest("ATL", "CUN", DateTime.Now, DateTime.Now.AddDays(1)));
 
 Console.WriteLine("Hello, World!");
