7775969d0aee508539e3f567e0067e505ff4e1b8
Naser <naser.r@gmail.com>
2024-05-29 22:26:42 +0330

    Change FlightNumber type to integer in Flight class
    Changed the data type of FlightNumber property in the Flight class from string to int. Also modified the parameter type in the class constructor accordingly and updated the guard clause for FlightNumber to Guard.Against.Zero.

--- src/TravelSample.Domain/Entities/Flight.cs
+++ src/TravelSample.Domain/Entities/Flight.cs
@@ -2,10 +2,10 @@
 
 namespace TravelSample.Domain.Entities;
 
-public class Flight(string airline, string flightNumber, string departureTime, string arrivalTime, decimal price)
+public class Flight(string airline, int flightNumber, string departureTime, string arrivalTime, decimal price)
 {
     public string Airline { get; init; } = Guard.Against.NullOrEmpty(airline, nameof(airline));
-    public string FlightNumber { get; init; } = Guard.Against.NullOrEmpty(flightNumber, nameof(flightNumber));
+    public int FlightNumber { get; init; } = Guard.Against.Zero(flightNumber, nameof(flightNumber));
     public string DepartureTime { get; init; } = Guard.Against.NullOrEmpty(departureTime, nameof(departureTime));
     public string ArrivalTime { get; init; } = Guard.Against.NullOrEmpty(arrivalTime, nameof(arrivalTime));
     public decimal Price { get; init; } = Guard.Against.Zero(price, nameof(price));
