6eddbd848071ea321f5ba68a5362c1224d52416b
Naser <naser.r@gmail.com>
2024-05-29 22:28:57 +0330

    Implement flight search functionality in TravelService
    Added code to integrate the Sabre service for flight search in the TravelService. This implementation includes handling API and other exceptions. It uses the Sabre service to search flights and iteratively construct a list of Flight objects, constructed with data from the API response. The SearchHotelAsync method remains unimplemented.

--- src/TravelSample.Infra/Services/TravelService.cs
+++ src/TravelSample.Infra/Services/TravelService.cs
@@ -1,17 +1,55 @@
-﻿using TravelSample.Domain.Abstractions;
+﻿using System.Globalization;
+using Refit;
+using TravelSample.Domain.Abstractions;
 using TravelSample.Domain.Base.Results;
 using TravelSample.Domain.Entities;
+using TravelSample.Domain.Errors;
+using TravelSample.Infra.Providers;
 
 namespace TravelSample.Infra.Services;
 
-public class TravelService : ITravelService
+public class TravelService(ISabreService sabreService/*, IAmadeusService amadeusService*/) : ITravelService
 {
-    public Task<Result<List<Flight>>> SearchFlightsAsync(string from, string to, DateTime departureDate, DateTime returnDate)
+    public async Task<Result<List<Flight>?>> SearchFlightsAsync(string origin, string destination,
+        DateTime departureDate, DateTime returnDate)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var searchResponse = await sabreService.SearchFlight(
+                origin,
+                destination,
+                departureDate.ToString("yyyy-MM-dd"),
+                returnDate.ToString("yyyy-MM-dd"));
+
+            var response = new List<Flight>();
+            foreach (var pricedItinerary in searchResponse.PricedItineraries)
+            {
+                var flightSegment = pricedItinerary.AirItinerary.OriginDestinationOptions.OriginDestinationOption.First().FlightSegments;
+
+                var flight = new Flight(
+                    pricedItinerary.TpaExtensions.ValidatingCarrier.Code,
+                    flightSegment.First().FlightNumber,
+                    flightSegment.First().DepartureDateTime.ToString(CultureInfo.InvariantCulture),
+                    flightSegment.First().ArrivalDateTime.ToString(CultureInfo.InvariantCulture),
+                    pricedItinerary.AirItineraryPricingInfo.ItinTotalFare.TotalFare.Amount
+                );
+
+                response.Add(flight);
+            }
+
+            return response;
+        }
+        catch (ApiException e)
+        {
+            return Result.Failure<List<Flight>>(SharedErrors.ProviderError(e.Content ?? string.Empty));
+        }
+        catch (Exception e)
+        {
+            return Result.Failure<List<Flight>>(SharedErrors.UnhandledError(e.Message));
+        }
     }
 
-    public Task<Result<List<Hotel>>> SearchHotelAsync(string city, DateTime checkIn, int rooms)
+    public Task<Result<List<Hotel>?>> SearchHotelAsync(string city, DateTime checkIn, int rooms)
     {
         throw new NotImplementedException();
     }
