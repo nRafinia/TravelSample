97492631b7b9b0aa3fd5da9193175789aa79d396
Naser <naser.r@gmail.com>
2024-05-29 20:00:24 +0330

    Add Sabre service interfaces and authentication handler
    Implemented the initial structure for the Sabre service integration, including the `ISabreService` and `ISabreTokenService` interfaces, and the skeleton for the `SabreAuthenticationHandler`. This setup will serve as the foundation for the upcoming development of the specific Sabre API's calls and associated authentication routines.

---
+++ src/TravelSample.Infra/Handler/SabreAuthenticationHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace TravelSample.Infra.Handler;
+
+public class SabreAuthenticationHandler:DelegatingHandler
+{
+    protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+        CancellationToken cancellationToken)
+    {
+        
+        return await base.SendAsync(request, cancellationToken);
+    }
+}
