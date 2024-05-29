b8d8f5ca5849be537ad40f24404753206b7b625e
Naser <naser.r@gmail.com>
2024-05-29 19:46:09 +0330

    Add ConfigurationExt class in TravelSample.Infra.Extensions
    A new class named ConfigurationExt has been added to the project, specifically in the TravelSample.Infra.Extensions namespace. This class provides a method for registering and retrieving configurations, helping reduce redundancy and enhance readability in our configuration management code.

---
+++ src/TravelSample.Infra/Extensions/ConfigurationExt.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TravelSample.Infra.Extensions;
+
+public static class ConfigurationExt
+{
+    public static T RegisterAndGetConfiguration<T>(this IConfiguration configuration, IServiceCollection services,
+        string sectionName)
+        where T : class, new()
+    {
+        var config = new T();
+        var section = configuration.GetSection(sectionName);
+        if (!section.Exists())
+        {
+            return config;
+        }
+        
+        section.Bind(config);
+        services.Configure<T>(section);
+
+        return config;
+    }
+}
