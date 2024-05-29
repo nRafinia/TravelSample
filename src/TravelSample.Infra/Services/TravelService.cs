94449c7a2c8b8151e497c9334975cdf4e93d88cc
Naser <naser.r@gmail.com>
2024-05-29 20:00:46 +0330

    Add TravelService class to Infrastructure layer
    Newly implemented `TravelService` class serves as part of the infrastructure services and provides implementation for `ITravelService`. This class currently includes two methods, `SearchFlightsAsync` and `SearchHotelAsync`,

---
+++ src/TravelSample.Infra/Services/TravelService.cs
@@ -0,0 +1,18 @@
+﻿using TravelSample.Domain.Abstractions;
+using TravelSample.Domain.Base.Results;
+using TravelSample.Domain.Entities;
+
+namespace TravelSample.Infra.Services;
+
+public class TravelService : ITravelService
+{
+    public Task<Result<List<Flight>>> SearchFlightsAsync(string from, string to, DateTime departureDate, DateTime returnDate)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Result<List<Hotel>>> SearchHotelAsync(string city, DateTime checkIn, int rooms)
+    {
+        throw new NotImplementedException();
+    }
+}
