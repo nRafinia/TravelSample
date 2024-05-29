750fe9a8b46cd8851512570c48b8af3df78ee761
Naser <naser.r@gmail.com>
2024-05-29 22:28:13 +0330

    Update return types of ITravelService methods
    The return types of methods in ITravelService interface, specifically SearchFlightsAsync and SearchHotelsAsync, have been updated to possibly return null. This allows for the possibility of no results being found during a flight or hotel search.

--- src/TravelSample.Domain/Abstractions/ITravelService.cs
+++ src/TravelSample.Domain/Abstractions/ITravelService.cs
@@ -5,6 +5,6 @@ namespace TravelSample.Domain.Abstractions;
 
 public interface ITravelService
 {
-    Task<Result<List<Flight>>> SearchFlightsAsync(string from, string to, DateTime departureDate, DateTime returnDate);
-    Task<Result<List<Hotel>>> SearchHotelAsync(string city, DateTime checkIn, int rooms);
+    Task<Result<List<Flight>?>> SearchFlightsAsync(string from, string to, DateTime departureDate, DateTime returnDate);
+    Task<Result<List<Hotel>?>> SearchHotelAsync(string city, DateTime checkIn, int rooms);
 }
