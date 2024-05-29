e4a92a882067e24c2f051d670a1709f619deef44
Naser <naser.r@gmail.com>
2024-05-29 22:28:35 +0330

    Add ProviderError and UnhandledError methods to SharedErrors class
    The SharedErrors class has been extended to include two new methods: ProviderError and UnhandledError. These methods enable more specific error handling, allowing pinpointing if an error occurs within the Provider or if there's an unhandled error within the application.

--- src/TravelSample.Domain/Errors/SharedErrors.cs
+++ src/TravelSample.Domain/Errors/SharedErrors.cs
@@ -1,10 +1,14 @@
-﻿
-
-using TravelSample.Domain.Base;
+﻿using TravelSample.Domain.Base;
 
 namespace TravelSample.Domain.Errors;
 
 public static class SharedErrors
 {
     public static readonly Error RequestInvalid = new Error("Request.Invalid", "Request is invalid");
+
+    public static Error ProviderError(string errorMessage) =>
+        new Error("Provider.Error", $"Provider error={errorMessage}");
+    
+    public static Error UnhandledError(string errorMessage) =>
+        new Error("Internal.Error", $"Unhandled error={errorMessage}");
 }
