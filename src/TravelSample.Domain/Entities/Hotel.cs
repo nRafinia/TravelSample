9fe23ccb6f892353623de20b333774e6f6196651
Naser <naser.r@gmail.com>
2024-05-30 09:52:55 +0330

    Update validation logic in Hotel entity
    Updated the validation rules for the Address, Rating, and Price properties in the Hotel entity. Removed the guard clause against null values for Address property. The validation for Rating and Price are now ensuring that their values are not negative, instead of not being zero.

--- src/TravelSample.Domain/Entities/Hotel.cs
+++ src/TravelSample.Domain/Entities/Hotel.cs
@@ -6,7 +6,7 @@ public class Hotel(string name, bool availability, string address, float rating,
 {
     public string Name { get; init; } = Guard.Against.NullOrEmpty(name, nameof(name));
     public bool Availability { get; init; } = availability;
-    public string Address { get; init; } = Guard.Against.NullOrEmpty(address, nameof(address));
-    public float Rating { get; init; } = Guard.Against.Zero(rating, nameof(rating));
-    public decimal Price { get; init; } = Guard.Against.Zero(price, nameof(price));
+    public string Address { get; init; } = address;
+    public float Rating { get; init; } = Guard.Against.Negative(rating, nameof(rating));
+    public decimal Price { get; init; } = Guard.Against.Negative(price, nameof(price));
 }
