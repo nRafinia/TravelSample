f6857382c98fa2d4a7d8ca57bfdaeab1cab6315c
Naser <naser.r@gmail.com>
2024-05-30 11:45:56 +0330

    Add method to customize error messages in SharedErrors
    A new static method `RequestInvalidWithMessage` has been added to the `SharedErrors` class. This method allows for generating custom 'Request.Invalid' errors with a detailed message.

--- src/TravelSample.Domain/Errors/SharedErrors.cs
+++ src/TravelSample.Domain/Errors/SharedErrors.cs
@@ -5,6 +5,7 @@ namespace TravelSample.Domain.Errors;
 public static class SharedErrors
 {
     public static readonly Error RequestInvalid = new Error("Request.Invalid", "Request is invalid");
+    public static Error RequestInvalidWithMessage(string message) => new Error("Request.Invalid", message);
 
     public static Error ProviderError(string errorMessage) =>
         new Error("Provider.Error", $"Provider error={errorMessage}");
