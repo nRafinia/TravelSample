011a18c8eee94a4a9f72b4cefb844f722f0e14f9
Naser <naser.r@gmail.com>
2024-05-30 11:46:38 +0330

    Add minimum length validation for city in SearchHotelRequest
    Extended the SearchHotelRequestValidation class to include a minimum length requirement of 3 characters for the 'City' field. This change ensures that the city name provided by users during hotel search is valid and helps to improve data quality.

--- src/TravelSample.Application/Models/SearchHotels/SearchHotelRequestValidation.cs
+++ src/TravelSample.Application/Models/SearchHotels/SearchHotelRequestValidation.cs
@@ -6,7 +6,7 @@ public class SearchHotelRequestValidation : AbstractValidator<SearchHotelRequest
 {
     public SearchHotelRequestValidation()
     {
-        RuleFor(f => f.City).NotEmpty();
+        RuleFor(f => f.City).NotEmpty().Length(3);
         RuleFor(f => f.CheckIn).GreaterThanOrEqualTo(DateTime.Now.Date);
         RuleFor(f => f.Rooms).GreaterThan(0);
     }
