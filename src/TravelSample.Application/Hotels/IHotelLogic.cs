0f0c3fca8c5e4c6dba0d30c74a703bf8b4366c9c
Naser <naser.r@gmail.com>
2024-05-30 09:53:51 +0330

    Add search method in IHotelLogic interface
    A new method, 'Search', has been added to the IHotelLogic interface, which allows for hotel search functionality depending on the request. This addition forms part of efforts to extend hotel-related functionalities.

--- src/TravelSample.Application/Hotels/IHotelLogic.cs
+++ src/TravelSample.Application/Hotels/IHotelLogic.cs
@@ -1,5 +1,8 @@
-﻿namespace TravelSample.Application.Hotels;
+﻿using TravelSample.Application.Models.SearchHotels;
+
+namespace TravelSample.Application.Hotels;
 
 public interface IHotelLogic
 {
+    Task<Result<List<Hotel>?>> Search(SearchHotelRequest request);
 }
