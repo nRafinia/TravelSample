1c5fa48d1323b9755bc3b2698f9f7b3b8a6a6918
Naser <naser.r@gmail.com>
2024-05-30 11:46:06 +0330

    Add FluentValidation extension for error handling
    This commit introduces a FluentValidation extension named FluentValidationExt. It contains a function 'ToError' that converts a list of ValidationFailures into a detailed error message. This extension will help improve error handling in the TravelSample application by providing clearer messages about validation failures.

---
+++ src/TravelSample.Application/Extensions/FluentValidationExt.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation.Results;
+using TravelSample.Domain.Base;
+using TravelSample.Domain.Errors;
+
+namespace TravelSample.Application.Extensions;
+
+public static class FluentValidationExt
+{
+    public static Error ToError(this List<ValidationFailure> failures)
+    {
+        var message = string.Join(Environment.NewLine, failures.Select(failure => $"{failure.PropertyName}={failure.ErrorMessage}"));
+        return SharedErrors.RequestInvalidWithMessage(message);
+    }
+}
